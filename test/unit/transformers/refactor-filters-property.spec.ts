ebffbbf8686f66b0e19cf923b57a79b13ae23cd1
harttle <yangjvn@126.com>
2020-03-31 14:44:34 +0800

    test: cover refactor-filters-property, 97.75%->98.2%

---
+++ test/unit/transformers/refactor-filters-property.spec.ts
@@ -0,0 +1,113 @@
+import { refactorFiltersProperty } from '../../../src/transformers/refactor-filters-property'
+import { PropertyDeclaration, Project } from 'ts-morph'
+
+describe('refactor filters property', function () {
+    const proj = new Project()
+
+    it('should skip if filters property does not have initializer', function () {
+        const sourceFile = proj.createSourceFile(
+            '/tmp/not-exist-file', `
+            class Foo {
+                filters
+            } `,
+            { overwrite: true }
+        )
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+                filters`)
+    })
+
+    it('should skip refactor if it is not object literal', function () {
+        const sourceFile = proj.createSourceFile('/tmp/not-exist-file', `
+        class Foo {
+            filters = false
+        }
+        `, { overwrite: true })
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+            filters = false`)
+    })
+
+    it('should replace property initializer', function () {
+        const sourceFile = proj.createSourceFile('/tmp/not-exist-file', `
+        class Foo {
+            filters = {
+                foo: function () {
+                    return this.data.get('bar')
+                }
+            }
+        }
+        `, { overwrite: true })
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+            filters = {
+                foo: function (sanssrSelf: Foo) {
+                    return sanssrSelf.data.get('bar')
+                }
+            }`)
+    })
+
+    it('should replace method declaration', function () {
+        const sourceFile = proj.createSourceFile('/tmp/not-exist-file', `
+        class Foo {
+            filters = {
+                foo () {
+                    return this.data.get('bar')
+                }
+            }
+        }
+        `, { overwrite: true })
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+            filters = {
+                foo (sanssrSelf: Foo) {
+                    return sanssrSelf.data.get('bar')
+                }
+            }`)
+    })
+
+    it('should replace method declaration', function () {
+        const sourceFile = proj.createSourceFile('/tmp/not-exist-file', `
+        class Foo {
+            filters = {
+                foo () {
+                    return this.data.get('bar')
+                }
+            }
+        }
+        `, { overwrite: true })
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+            filters = {
+                foo (sanssrSelf: Foo) {
+                    return sanssrSelf.data.get('bar')
+                }
+            }`)
+    })
+
+    it('should replace all this occurences', function () {
+        const sourceFile = proj.createSourceFile('/tmp/not-exist-file', `
+        class Foo {
+            bar() { return 'bar' }
+            filters = {
+                foo () {
+                    return this.bar()
+                }
+            }
+        }
+        `, { overwrite: true })
+        const prop = sourceFile.getClass('Foo').getProperty('filters')
+        refactorFiltersProperty(prop)
+        expect(prop.getFullText()).toEqual(`
+            filters = {
+                foo (sanssrSelf: Foo) {
+                    return sanssrSelf.bar()
+                }
+            }`)
+    })
+})
