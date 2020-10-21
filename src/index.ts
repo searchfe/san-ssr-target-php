c444ee488dcd56ecdce948e2495fd6c598f92a03
yangqi07 <yangqi07@baidu.com>
2020-10-22 00:46:48 +0800

    fix: 传递给ts2php的helperNamespace参数需要是绝对路径

--- src/index.ts
+++ src/index.ts
@@ -114,7 +114,7 @@ export default class ToPHPCompiler implements Compiler {
         emitter.writeLines(this.phpTranspiler.compile(
             sourceFile.tsSourceFile,
             { ...modules, ...options.modules },
-            helpersNS,
+            helpersNS.substr(0, 1) === '\\' ? helpersNS : '\\' + helpersNS,
             options.getModuleNamespace
         ))
     }
